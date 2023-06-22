4d0b8f3f941e7b16f6aab6d6a39777c61c5285cc
ibrotech01 <45831834+ibrotech01@users.noreply.github.com>
2023-06-22 02:23:27 +0100

    amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-""" State Module for the HBNB project """
-from os import getenv
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+"""This is the amenity class"""
+from models.base_model import BaseModel, Base, os_type_storage
+from sqlalchemy import Column, Float, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
+import models
 
 
 class Amenity(BaseModel, Base):
-    """class for amenities"""
-    __tablename__ = 'amenities'
-    if getenv("HBNB_TYPE_STORAGE") == "db":
+    """This is the class for Amenity
+    Attributes:
+        name: input name
+    """
+
+    if os_type_storage == "db":
+        __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
-        place_amenities = relationship("Place", secondary="place_amenity",
-                                       back_populates="amenities")
+#        place_amenities = relationship("Place",
+#                                       secondary=place_amenity,
+#                                       backref="amenities")
     else:
         name = ""
