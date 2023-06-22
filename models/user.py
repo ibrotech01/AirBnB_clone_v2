15927159142d0d6b54bf73386c3293af01a597a0
ibrotech01 <45831834+ibrotech01@users.noreply.github.com>
2023-06-22 02:15:54 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+"""This is the user class"""
+from models.base_model import BaseModel, Base, os_type_storage
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from os import getenv
 
 
 class User(BaseModel, Base):
-    """ This class defines a user by various attributes """
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
+
+    if os_type_storage == "db":
+        __tablename__ = "users"
+        email = Column(String(128),  nullable=False)
+        password = Column(String(128),  nullable=False)
+        first_name = Column(String(128),  nullable=True)
+        last_name = Column(String(128),  nullable=True)
+        places = relationship('Place', cascade="all, delete", backref='user')
+        reviews = relationship('Review', cascade="all, delete", backref='user')
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
