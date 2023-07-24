3ce577e2786b7724b8dcd4d0dc269e1f367ba735
ibrotech01 <45831834+ibrotech01@users.noreply.github.com>
2023-07-24 23:09:10 +0100

    8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,96 +1,32 @@
 #!/usr/bin/python3
-''' script that starts a Flask web application '''
-from flask import Flask, escape, render_template
-from models import storage
-from models.base_model import os_type_storage
+"""Start web application with two routings
+"""
 
+from models import storage
 from models.state import State
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_route():
-    ''' renders a message '''
-    return "Hello HBNB!"
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    ''' renders a message '''
-    return "HBNB"
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def c_param(text):
-    ''' renders a message with a q param '''
-    return "C %s" % escape(text.replace('_', ' '))
-
-
-@app.route('/python/', defaults={'text': 'is_cool'})
-@app.route('/python/<text>/', strict_slashes=False)
-def python_param(text):
-    ''' renders a message with a q param
-        and defaults value
-    '''
-    return "Python %s" % escape(text.replace('_', ' '))
-
-
-@app.route('/number/<int:n>/', strict_slashes=False)
-def number_param(n):
-    ''' renders a message with a q param
-        and displays value only if n is an integer
-    '''
-    return "%d is a number" % n
-
-
-@app.route('/number_template/<int:n>/', strict_slashes=False)
-def number_template(n):
-    ''' renders a message with a q param
-        and displays value only if n is an integer
-        renders an tml template
-    '''
-    return render_template('5-number.html', n=n)
-
-
-@app.route('/number_odd_or_even/<int:n>/', strict_slashes=False)
-def number_odd_or_even(n):
-    ''' renders a message with a q param
-        and displays value only if n is an integer
-        renders an tml template
-    '''
-    return render_template('6-number_odd_or_even.html', n=n)
-
-
-@app.route('/states_list/', strict_slashes=False)
+@app.route('/cities_by_states')
 def states_list():
-    ''' displays a list of all State
-        objects present in DBStorage sorted by name
-        renders an html template
-    '''
-    states_objs = storage.all('State').values()
-    return render_template('7-states_list.html', states=states_objs)
+    """Render template with states
+    """
+    path = '8-cities_by_states.html'
+    states = storage.all(State)
 
-
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    ''' displays a list of all State
-        objects present in DBStorage
-        or FileStorage
-        sorted by name
-        renders an html template
-    '''
-    if os_type_storage == 'db':
-        states_objs = storage.all('State').values()
-    else:
-        states_objs = storage.all(State).values()
-    return render_template('8-cities_by_states.html', states=states_objs)
+    # sort State object alphabetically by name
+    # sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template(path, states=states)
 
 
 @app.teardown_appcontext
-def close_session_db(exit):
-    ''' remove the current SQLAlchemy Session '''
+def app_teardown(arg=None):
+    """Clean-up session
+    """
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
