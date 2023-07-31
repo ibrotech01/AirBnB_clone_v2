dd281c8c422aa84d8b811f1053f09d8b677e246f
ibrotech01 <45831834+ibrotech01@users.noreply.github.com>
2023-07-31 01:55:45 +0100

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
-"""
-Fabric script generates .tgz archive of all in web_static/ using func 'do_pack'
-Usage: fab -f 1-pack_web_static.py do_pack
-
-All files in the folder web_static must be added to the final archive
-All archives must be stored in the folder 'versions' (create folder if none)
-Create archive "web_static_<year><month><day><hour><minute><second>.tgz"
-The function do_pack must return the archive path, else return None
-"""
-from fabric.api import local
-from time import strftime
+"""A module for Fabric script that generates a .tgz archive."""
+import os
+from datetime import datetime
+from fabric.api import local, runs_once
 
 
+@runs_once
 def do_pack():
-    """generate .tgz archive of web_static/ folder"""
-    timenow = strftime("%Y%M%d%H%M%S")
+    """Archives the static files."""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    d_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        d_time.year,
+        d_time.month,
+        d_time.day,
+        d_time.hour,
+        d_time.minute,
+        d_time.second
+    )
     try:
-        local("mkdir -p versions")
-        filename = "versions/web_static_{}.tgz".format(timenow)
-        local("tar -cvzf {} web_static/".format(filename))
-        return filename
-    except:
-        return None
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, size))
+    except Exception:
+        output = None
+    return output
